48cb80d3ad2d428a8b9a2266e20d32909d52e441
Abdallah Arabyat <48789766+arabyat@users.noreply.github.com>
2020-04-13 03:10:48 +0300

    Update build.sh
    Updated SEVER_NAME

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 echo "Building a webpage..."
 
 # Declare and initialize values
-SERVER_NAME=$HOSTNAME
+SERVER_NAME=$SEVER_NAME
 
 # Replace values in index.html
 ## TODO: Replace {SEVER_NAME} with $SEVER_NAME
