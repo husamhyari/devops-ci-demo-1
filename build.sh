3a56cb5d733cbdf713a81137b6d5020bf08c6004
Abdallah Arabyat <48789766+arabyat@users.noreply.github.com>
2020-04-15 00:52:18 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "Building a webpage..."
 SERVER_NAME=$SEVER_NAME
 echo "This is $HOSTNAME"
 echo $(pwd)
-sed -i 's/{HOSTNAME}/$HOSTNAME/g' web/index.html
+sed -i "s/{HOSTNAME}/$HOSTNAME/g" web/index.html
 
 # Replace values in index.html
 ## TODO: Replace {SEVER_NAME} with $SEVER_NAME
