612f0e1dac83feca2ee2ed9a66f0d2f13c382cab
Abdallah Arabyat <48789766+arabyat@users.noreply.github.com>
2020-04-15 01:43:12 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,16 +9,16 @@ SERVER_NAME=$SEVER_NAME
 echo "This is $HOSTNAME"
 echo $(pwd)
 dir=$pwd
-git branch 
-git rev-parse --abbrev-ref HEAD
-git status
-git config --get remote.origin.url
+ 
+
+
 echo $(git branch)
 sed -i "s/{HOSTNAME}/$HOSTNAME/g" web/index.html
 sed -i "s/{USER}/$USER/g" web/index.html
 sed -i "s/{GIT_BRANCH}/$(git rev-parse --abbrev-ref HEAD)/g" web/index.html
+sed -i "s/{GIT_REPO}/$(git config --get remote.origin.url)/g" web/index.html
 sed -i "s/{AZURE_VARIABLE}/$TEST/g" web/index.html
-sed -i "s/{PWD}/$(pwd)/g" web/index.html
+sed -i "s/{PWD}/$dir/g" web/index.html
 
 
 
