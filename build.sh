4ba7f461c71ec55a925c7db066f86b8c33a1c5d9
Abdallah Arabyat <48789766+arabyat@users.noreply.github.com>
2020-04-15 01:45:38 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,8 +15,8 @@ dir=$pwd
 echo $(git branch)
 sed -i "s/{HOSTNAME}/$HOSTNAME/g" web/index.html
 sed -i "s/{USER}/$USER/g" web/index.html
-sed -i "s/{GIT_BRANCH}/$(git rev-parse --abbrev-ref HEAD)/g" web/index.html
-sed -i "s/{GIT_REPO}/$(git config --get remote.origin.url)/g" web/index.html
+#sed -i "s/{GIT_BRANCH}/$(git rev-parse --abbrev-ref HEAD)/g" web/index.html
+#sed -i "s/{GIT_REPO}/$(git config --get remote.origin.url)/g" web/index.html
 sed -i "s/{AZURE_VARIABLE}/$TEST/g" web/index.html
 sed -i "s/{PWD}/$dir/g" web/index.html
 
